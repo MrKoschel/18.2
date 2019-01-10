52fb9a077dda3410e45cfa7abd871bbaa29466c3
MrKoschel <38735374+MrKoschel@users.noreply.github.com>
2019-01-10 12:17:36 +0100

    Change script.js

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
-process.stdin.setEncoding('utf-8');
+"use strict";
 
-var element = React.createElement('div', {}, 'Hello world!');
+var movies = [
+    {
+        id: 1,
+        title: 'Harry Potter',
+        desc: 'Film o czarodzieju',
+        image: 'http://fandoms.pl/king-include/buzzupload/harrypotter_wb_f4_harrypottermidshot_promo_080615_port-9800970685.jpg'
+  },
+    {
+        id: 2,
+        title: 'Król Lew',
+        desc: 'Film o królu sawanny',
+        image: 'http://fandoms.pl/king-include/buzzupload/harrypotter_wb_f4_harrypottermidshot_promo_080615_port-9800970685.jpg'
+  },
+    {
+        id: 3,
+        title: 'Batman',
+        desc: 'Film o superbohaterze',
+        image: 'http://fandoms.pl/king-include/buzzupload/harrypotter_wb_f4_harrypottermidshot_promo_080615_port-9800970685.jpg'
+  },
+    {
+        id: 4,
+        title: 'Kubuś Puchatek',
+        desc: 'Film o misiu',
+        image: 'http://fandoms.pl/king-include/buzzupload/harrypotter_wb_f4_harrypottermidshot_promo_080615_port-9800970685.jpg'
+  }
+];
+
+var moviesElements = movies.map(function (movie) {
+    return React.createElement('li', {
+            key: movie.id
+        },
+        React.createElement('h2', {}, movie.title),
+        React.createElement('p', {}, movie.desc),
+        React.createElement('img', {src: movie.image} )
+    );
+});
+
+var element =
+    React.createElement('div', {},
+        React.createElement('h1', {}, 'Lista filmów'),
+        React.createElement('ul', {}, moviesElements)
+    );
+
 ReactDOM.render(element, document.getElementById('app'));
